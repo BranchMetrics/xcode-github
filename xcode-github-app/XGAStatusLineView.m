4cbdadeca8c23218e23c7b2a732dd39dbf5e09af
Edward Smith <esmith@branch.io>
2018-05-10 17:06:52 -0700

    Save progress.

--- xcode-github-app/XGAStatusLineView.m
+++ xcode-github-app/XGAStatusLineView.m
@@ -24,6 +24,14 @@
     [[NSColor lightGrayColor] set];
     [line setLineWidth:0.5];
     [line stroke];
+
+    // Shadow line:
+    line = [NSBezierPath bezierPath];
+    [line moveToPoint:NSMakePoint(0.0, self.bounds.size.height-2)];
+    [line lineToPoint:NSMakePoint(self.bounds.size.width, self.self.bounds.size.height-2)];
+    [[[NSColor lightGrayColor] colorWithAlphaComponent:0.05] set];
+    [line setLineWidth:1.5];
+    [line stroke];
 }
 
 @end
